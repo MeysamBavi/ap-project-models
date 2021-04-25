dfcb5dd55fe405d26b6d827dc4a45da55b2ae554
Meysam Bavi <meybsky@gmail.com>
2021-04-25 21:40:57 +0430

    Add classes & ui subfolders

--- lib/models.dart
+++ lib/models.dart
@@ -1,1 +1,8 @@
-export 'src/Form.dart';
+export 'src/ui/Form.dart';
+export 'src/classes/food.dart';
+export 'src/classes/food_menu.dart';
+export 'src/classes/price.dart';
+export 'src/classes/restaurant.dart';
+export 'src/classes/serializable.dart';
+export 'src/classes/serializer.dart';
+export 'src/classes/server.dart';
