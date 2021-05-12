ef446aa3137d564d196e9b52e9e939f4f30d006f
Meysam Bavi <meybsky@gmail.com>
2021-05-13 03:36:50 +0430

    Added a method to supply info for sale statistic

--- lib/src/classes/owner_account.dart
+++ lib/src/classes/owner_account.dart
@@ -1,7 +1,7 @@
 import 'account.dart';
-import 'address.dart';
 import 'server.dart';
 import 'restaurant.dart';
+import 'order.dart';
 
 class OwnerAccount extends Account {
   final Restaurant restaurant;
@@ -11,5 +11,21 @@ class OwnerAccount extends Account {
     required this.restaurant,
     required Server server
 }) : super(phoneNumber: phoneNumber, server: server);
+
+  List<int> calculateCountPrice(DateTime? time, bool previousOrders) {
+    var orders = previousOrders ? super.previousOrders : super.activeOrders;
+    int count = 0, price = 0;
+    orders.forEach((element) {
+      if (time == null || _areSameDay(element.time, time)) {
+        element.items.forEach((key, value) => count += value);
+        price += element.totalCost.toInt();
+      }
+    });
+    return [count, price];
+  }
+
+  bool _areSameDay(DateTime a, DateTime b) {
+    return (a.day == b.day) && (a.month == b.month) && (a.year == b.year);
+  }
 
 }
