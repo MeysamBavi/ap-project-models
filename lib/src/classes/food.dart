f64ec6526daf555b283cf6dbd7d6b96bda5e276a
Meysam Bavi <meybsky@gmail.com>
2021-07-10 22:33:06 +0430

    Remove calls to server.edit

--- lib/src/classes/food.dart
+++ lib/src/classes/food.dart
@@ -7,13 +7,13 @@ import 'package:flutter/material.dart';
 
 class Food with Serializable implements Editable {
 
-  String _name;
-  Price _price;
-  String _description;
-  bool _isAvailable;
-  FoodCategory _category;
+  String name;
+  Price price;
+  String description;
+  bool isAvailable;
+  FoodCategory category;
   Server _server;
-  Image _image;
+  Image image;
 
   Food({
     required String name,
@@ -24,22 +24,22 @@ class Food with Serializable implements Editable {
     bool isAvailable = true,
     required Server server
   }) :
-        _name = name,
-        _category = category,
-        _price = price,
-        _image = image ?? Image.asset('assets/default_food.jpg' , package: 'models',),
-        _description = description ?? '',
-        _isAvailable = isAvailable,
+        name = name,
+        category = category,
+        price = price,
+        image = image ?? Image.asset('assets/default_food.jpg' , package: 'models',),
+        description = description ?? '',
+        isAvailable = isAvailable,
         _server = server;
 
   Food.fromJson(Map<String, dynamic> json, Server server):
         _server = server,
-        _name = json['name'],
-        _price = Price(int.parse(json['price'])),
-        _description = json['description'],
-        _isAvailable = json['isAvailable'],
-        _category = Food.toCategory(json['category'])!,
-        _image = Image.asset('assets/default_food.jpg' , package: 'models',)
+        name = json['name'],
+        price = Price(int.parse(json['price'])),
+        description = json['description'],
+        isAvailable = json['isAvailable'],
+        category = Food.toCategory(json['category'])!,
+        image = Image.asset('assets/default_food.jpg' , package: 'models',)
   {
     id = json['ID'];
   }
@@ -47,60 +47,17 @@ class Food with Serializable implements Editable {
 
   Map<String, dynamic> toJson() => {
     'ID' : id,
-    'name' : _name,
-    'price' : _price.toInt().toString(),
-    'description' : _description,
-    'isAvailable' : _isAvailable,
-    'category' : _category.toString(),
+    'name' : name,
+    'price' : price.toInt().toString(),
+    'description' : description,
+    'isAvailable' : isAvailable,
+    'category' : category.toString(),
   };
-
-  String get name => _name;
-
-  Price get price => _price;
-
-  String get description => _description;
-
-  bool get isAvailable => _isAvailable;
-
-  FoodCategory get category => _category;
-
-  Image get image => _image;
 
   @override
   Server get server => _server;
 
-  FoodData toFoodData() => FoodData(_name, id!, _price);
-
-  set category(FoodCategory value) {
-    _category = value;
-    server.edit(this);
-  }
-
-  set isAvailable(bool value) {
-    _isAvailable = value;
-    server.edit(this);
-  }
-
-  set description(String value) {
-    _description = value;
-    server.edit(this);
-  }
-
-  set price(Price value) {
-    _price = value;
-    server.edit(this);
-  }
-
-  set name(String value) {
-    _name = value;
-    server.edit(this);
-  }
-
-  set image(Image image)
-  {
-    _image = image;
-    server.edit(this);
-  }
+  FoodData toFoodData() => FoodData(name, id!, price);
 
   // converts the result of [FoodCategory].toString() back to [FoodCategory]
   static FoodCategory? toCategory(String foodCategory) {
