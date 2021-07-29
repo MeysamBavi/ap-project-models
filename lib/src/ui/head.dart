8d3489b8494e108d668598bacdf8cbf0d3a7a7e8
Meysam Bavi <meybsky@gmail.com>
2021-07-30 02:48:48 +0430

    Add serverPointer and offline mode

--- lib/src/ui/head.dart
+++ lib/src/ui/head.dart
@@ -5,26 +5,37 @@ import '../../src/classes/owner_server.dart';
 
 class Head extends InheritedWidget {
 
-  final Server server;
+  final _ServerPointer _serverPointer;
+  final bool _isForUser;
+
+  Server get server => _serverPointer.value;
 
   UserServer get userServer {
+    var server = _serverPointer.value;
     if (server is! UserServer) throw Exception('Illegal use of getter for UserServer.');
-    return server as UserServer;
+    return server;
   }
 
   OwnerServer get ownerServer {
+    var server = _serverPointer.value;
     if (server is! OwnerServer) throw Exception('Illegal use of getter for OwnerServer.');
-    return server as OwnerServer;
+    return server;
   }
 
   Head({
     Key? key,
     required Widget child,
-    required this.server,
-})  : super(key: key, child: child);
+    required Server server,
+  })  : _serverPointer = _ServerPointer(server),
+        _isForUser = server is UserServer,
+        super(key: key, child: child);
 
   static Head of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<Head>()!;
+  }
+
+  set offlineMode(bool value) {
+    //TODO
   }
 
   @override
@@ -32,4 +43,9 @@ class Head extends InheritedWidget {
     return false;
   }
 
+}
+
+class _ServerPointer {
+  Server value;
+  _ServerPointer(this.value);
 }
