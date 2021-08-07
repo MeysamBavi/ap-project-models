feecbcc79e0902699d10f0095788d564ed63a11a
Meysam Bavi <meybsky@gmail.com>
2021-08-07 13:56:28 +0430

    Use card for comment widget

--- lib/src/ui/comment_widget.dart
+++ lib/src/ui/comment_widget.dart
@@ -24,54 +24,54 @@ class _CommentTileState extends State<CommentTile> {
     var comment = widget.comment;
     var isForOwner = widget.isForOwner;
 
-    return ExpansionTile(
-      title: Text(comment.title),
-      leading: buildScore(comment.score),
-      subtitle: Text(Strings.formatDate(comment.time), style: Theme.of(context).textTheme.caption,),
-      backgroundColor: Colors.grey[50],
-      collapsedBackgroundColor: Colors.white,
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width - 50,
-          margin: EdgeInsets.all(6.0),
-          child: Text(comment.message),
-          padding: EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Theme.of(context).primaryColorDark),
-            color: Colors.white
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-          child: Form(
-            key: _formKey,
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: isForOwner ? 'Enter reply' : 'No response',
-                icon: Icon(Icons.reply),
-              ),
-              readOnly: !isForOwner || comment.reply != null,
-              initialValue: comment.reply,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return Strings.get('empty-reply-error');
-                }
-              },
-              onSaved: (value) => comment.reply = value,
+    return Card(
+      child: ExpansionTile(
+        title: Text(comment.title),
+        leading: buildScore(comment.score),
+        subtitle: Text(Strings.formatDate(comment.time), style: Theme.of(context).textTheme.caption,),
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width - 50,
+            margin: EdgeInsets.all(6.0),
+            child: Text(comment.message, style: Theme.of(context).textTheme.bodyText1),
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Theme.of(context).primaryColorDark),
+              color: Colors.white
             ),
           ),
-        ),
-        if (isForOwner)
           Padding(
-            padding: const EdgeInsets.all(6.0),
-            child: TextButton(
-              child: Text(Strings.get('confirm')!),
-              onPressed: comment.reply == null ? confirmPressed : null,
+            padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+            child: Form(
+              key: _formKey,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: isForOwner ? Strings.get('enter-comment-reply-prompt')! : Strings.get('comment-no-response'),
+                  icon: Icon(Icons.reply),
+                ),
+                readOnly: !isForOwner || comment.reply != null,
+                initialValue: comment.reply,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return Strings.get('empty-reply-error');
+                  }
+                },
+                onSaved: (value) => comment.reply = value,
+              ),
             ),
           ),
-      ],
-      expandedCrossAxisAlignment: CrossAxisAlignment.center,
+          if (isForOwner)
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: TextButton(
+                child: Text(Strings.get('confirm')!),
+                onPressed: comment.reply == null ? confirmPressed : null,
+              ),
+            ),
+        ],
+        expandedCrossAxisAlignment: CrossAxisAlignment.center,
+      ),
     );
   }
 
